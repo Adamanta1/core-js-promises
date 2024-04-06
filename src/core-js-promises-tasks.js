4e3890092f4cad2580cef3ce65b68c5b7e43926e
adamanta1 <elena.brashkina2020@gmail.com>
2024-04-06 05:15:16 +0400

    feat: solve 2 last tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -119,8 +119,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) =>
+      result.status === 'rejected' ? null : result.value
+    )
+  );
 }
 
 /**
@@ -141,8 +145,18 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return new Promise((resolve) => {
+    const arr = [];
+    promises.map((promise, index) => {
+      promise.then((result) => {
+        arr[index] = result;
+        return result;
+      });
+      return promise;
+    });
+    setTimeout(() => resolve(arr.join('')), 10);
+  });
 }
 
 module.exports = {
